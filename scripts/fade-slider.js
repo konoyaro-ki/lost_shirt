b1d632776e0cd8270d115cfd5336e47e929ebb1b
konoyaro-ki <chepel1997@gmail.com>
2022-02-19 05:07:57 +0600

    Initial commit

---
+++ scripts/fade-slider.js
@@ -0,0 +1,17 @@
+(function(){
+
+    const slider = document.querySelectorAll('.fade-slider__item');
+    const activeClass = "fade-slider__item--visible";
+    let index = 0;
+
+    setInterval(function() {
+        slider[index].classList.remove(activeClass);
+        index++;
+
+        if (index + 1 > slider.length) {
+            index = 0;
+        }
+
+        slider[index].classList.add(activeClass);
+    }, 5000);
+})();
